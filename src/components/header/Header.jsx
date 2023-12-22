dd27a1790dfe14465c4d329900b89a2ff203b831
SoulAndBone <soulandbone@gmail.com>
2023-12-22 19:18:17 +0200

    header, added socials and scroll down on the side

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,26 +1,24 @@
 import React from 'react';
 import './header.css';
 import CTA from './CTA';
-import ME from './../../assets/portrait.jpg';
+import ME from './../../assets/me.png';
 import HeaderSocials from './HeaderSocials';
 
 const Header = () => {
   return (
       <header >
         <div className="container header__container">
-        <h5>Hello I'm</h5>
-        <h1>Gustavo Gikis</h1>
-        <h5 className="text-light">Fullstack Developer</h5>
-        <CTA/>
-        <HeaderSocials/>
+          <h5>Hello I'm</h5>
+          <h1>Gustavo Gikis</h1>
+          <h5 className="text-light">Fullstack Developer</h5>
+          <CTA/>
+          <HeaderSocials className='header__socials'/>
 
+          <div className="me">
+            <img src={ME} alt="me" className='profile_pic'/>
+          </div>
 
-      <div className="me">
-        <img src={ME} alt="" />
-      </div>
-
-      <a href="#contact" className='scroll__down'></a>
-
+          <a href="#contact" className='scroll__down'>Scroll Down</a>
         </div>
       </header>
 
