131da294e8a5664a7b92b744921e076076c3927f
SoulAndBone <soulandbone@gmail.com>
2023-12-25 08:19:50 +0200

    used map to generate individual <a> elements inside the navbar

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,7 +3,7 @@ import './contact.css';
 
 const Contact = () => {
   return (
-    <section id='section'>Contact</section>
+    <section id='contact'>Contact</section>
   )
 }
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -22,14 +22,33 @@ const Nav = () => {
     return getClassName;
   }
 
+  const navLinks = [
+    {href:'#', icon:<IoHomeOutline/> },
+    {href:'#about', icon:<CiUser/> },
+    {href:'#experience', icon:<IoBookOutline/> },
+    {href:'#services', icon:<RiServiceLine/> },
+    {href:'#contact', icon:<BiMessageDetail/> },
+  ];
+
 
   return (
     <nav>
-      <a href="#" className={checkHref('#')} onClick={handleClick}><IoHomeOutline/></a>
+      {/* <a href="#" className={checkHref('#')} onClick={handleClick}><IoHomeOutline/></a>
       <a href="#about" className={checkHref('#about')} onClick={handleClick} ><CiUser/></a>
       <a href="#experience"className={checkHref('#experience')}  onClick={handleClick}><IoBookOutline/></a>
       <a href="#services" className={checkHref('#services')}  onClick={handleClick}><RiServiceLine/></a>
-      <a href="#contact"className={checkHref('#contact')}  onClick={handleClick}><BiMessageDetail/></a>
+      <a href="#contact"className={checkHref('#contact')}  onClick={handleClick}><BiMessageDetail/></a> */}
+      {navLinks.map(link => (
+        <a 
+        id={link.href}
+        className={checkHref(link.href)}
+        href={link.href}
+        onClick={handleClick}
+        >
+          {link.icon}
+        </a>
+      ))}
+      
     </nav>
   )
 }
