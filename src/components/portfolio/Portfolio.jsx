c1f10f93485a716062d535f28914999f5a40fff4
SoulAndBone <soulandbone@gmail.com>
2023-12-28 19:38:38 +0200

    data for map added

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -7,6 +7,56 @@ import IMG4 from '../../assets/portfolio4.jpg';
 import IMG5 from '../../assets/portfolio5.png';
 import IMG6 from '../../assets/portfolio6.jpg';
 
+
+const data = [
+  {
+    id:1,
+    image: IMG1,
+    title: 'Crypto Currency Dashboard & Financial Visualization',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/shots/16673715-Crypto-currency-dashboards-and-financial-data-visualization'
+  },
+  {
+    id:2,
+    image: IMG2,
+    title: 'Chart templates & infographics in Figma',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/shots/16580766-Orion-UI-kit-Charts-templates-infographics-in-Figma'
+  },
+  {
+    id:3,
+    image: IMG3,
+    title: 'Figma dashboard UI kit for data design web apps',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/shots/17290917-Eclipse-Figma-dashboard-UI-kit-for-data-design-web-apps'
+  },
+  {
+    id:4,
+    image: IMG4,
+    title: 'Maintaining tasks and tracking progress',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/shots/16955822-Maintaining-tasks-and-tracking-progress'
+  },
+  {
+    id:5,
+    image: IMG5,
+    title: 'Charts templates & infographics in Figma',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/shots/16541289-Orion-UI-kit-Charts-templates-infographics-in-Figma'
+  },
+
+  {
+    id:6,
+    image: IMG6,
+    title: 'Charts templates & infographics in Figma',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/shots/15887665-Orion-UI-kit-Charts-templates-infographics-in-Figma'
+  },
+
+
+]
+
+
 const Portfolio = () => {
   return (
     <section id='portfolio'>
