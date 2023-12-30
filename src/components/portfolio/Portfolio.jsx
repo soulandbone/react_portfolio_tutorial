48adb7ec1eb75553ae284cba62521e1f21655a02
SoulAndBone <soulandbone@gmail.com>
2023-12-30 15:11:31 +0200

    solved an issue when the elements were not rendering properly on the map method, there was a missing div encapsulating everything

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -63,6 +63,9 @@ const Portfolio = () => {
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
 
+      
+
+      <div className="container portfolio__container">
       {data.map(element => {
         return  <article className="portfolio__item" id={element.id}>
         <div className="portfolio__item-image"> 
@@ -76,9 +79,6 @@ const Portfolio = () => {
          
       </article>
       })}
-
-      <div className="container portfolio__container">
-       
       
       
       </div>
